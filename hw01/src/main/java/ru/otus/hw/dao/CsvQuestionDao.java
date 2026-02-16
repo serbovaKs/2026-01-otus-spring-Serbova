eff68464d2fa258c16d568503d5cd4ad84eab7d5
Ksenya.Serbova <Ksenya.Serbova@inversion.ru>
2026-02-16 21:44:56 +0300

    hw01 Выполнение домашнего задания №1 (часть 1)

---
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,42 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        List<QuestionDto> beans;
+        var resource = getClass().getClassLoader().getResource(fileNameProvider.getTestFileName());
+        try (Reader reader = Files.newBufferedReader(Paths.get(Objects.requireNonNull(resource).toURI()))) {
+            beans = new CsvToBeanBuilder(reader)
+                    .withType(QuestionDto.class)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .build()
+                    .parse();
+        } catch (URISyntaxException | IOException e) {
+            throw new QuestionReadException(e.getMessage());
+        }
+        // Использовать CsvToBean
+        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+        // Использовать QuestionReadException
+        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+
+        return beans.stream().map(QuestionDto::toDomainObject).toList();
+    }
+}
---
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public void executeTest() {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        // Получить вопросы из дао и вывести их с вариантами ответов
+        var questions = questionDao.findAll();
+        questions.forEach(question -> {
+            ioService.printLine("");
+            ioService.printFormattedLine(question.text());
+            question.answers().forEach(answer -> ioService.printFormattedLine("*\t%s", answer.text()));
+        });
+    }
+}

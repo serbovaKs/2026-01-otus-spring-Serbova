e461146d5321c5552e2af7d19e2928a0c7c2da46
ksser <ks.serb@mail.ru>
2026-03-13 08:35:05 +0300

    Changes

---
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,48 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question: questions) {
+            var isAnswerValid = false;
+            ioService.printFormattedLine(question.text());
+            List<Answer> answers = question.answers();
+            int numCorrectAnswer = 0;
+            for (int index = 0; index < answers.size(); index++) {
+                ioService.printLine("%s - %s".formatted(index + 1, answers.get(index).text()));
+                if (answers.get(index).isCorrect()) {
+                    numCorrectAnswer = index + 1;
+                }
+            }
+            int numAnswer = ioService.readIntForRange(
+                    1,
+                    question.answers().size(),
+                    "Selected answer number is outside the allowed range"
+            );
+            isAnswerValid = numCorrectAnswer == numAnswer;
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+}

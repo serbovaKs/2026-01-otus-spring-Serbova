d142d68d98c26cdc2679f56a3b3c065bdcf9ccd4
ksser <ks.serb@mail.ru>
2026-06-24 11:57:01 +0300

    Исправление недочетов

--- hw07/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.repositories;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.hw.models.Comment;
 
@@ -8,6 +7,5 @@ import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 
-    @EntityGraph(attributePaths = "book")
     List<Comment> findByBookId(long bookId);
 }
